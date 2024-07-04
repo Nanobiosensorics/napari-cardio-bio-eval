fb12d46d386e0564d309f4c5f84b87680cf057a3
David <78443646+wittmajerd@users.noreply.github.com>
2024-07-04 12:26:43 +0200

    background selection + threading

--- src/napari_cardio_bio_eval/_widget.py
+++ src/napari_cardio_bio_eval/_widget.py
@@ -68,13 +68,15 @@ class CardioBioEvalWidget(QWidget):
         self.filterMethod.addItems(['mean', 'median'])
         self.layout.addRow(QLabel('Filter method:'), self.filterMethod)
 
-        self.backgroundSelector = QCheckBox(self)
-        self.layout.addRow(QLabel('Background selector:'), self.backgroundSelector)
-
         # Data loading button
         self.loadButton = QPushButton('Load and Preprocess Data', self)
         self.loadButton.clicked.connect(self.loadAndPreprocessData)
         self.layout.addRow(self.loadButton)
+
+        # Manual Background selection
+        self.backgroundSelectorButton = QPushButton('Manual Background Selection', self)
+        self.backgroundSelectorButton.clicked.connect(self.manualBackgroundSelection)
+        self.layout.addRow(self.backgroundSelectorButton)
 
         # Peak detection parameters
         self.neighbourhood_size = QSpinBox(self)
@@ -133,14 +135,13 @@ class CardioBioEvalWidget(QWidget):
 
 
     def openFileNameDialog(self):
-        # TODO check the selected directory for the needed files
+        # TODO check the selected directory for the needed files if neccecary (in data_load function it does)
         options = QFileDialog.Options()
         directory = QFileDialog.getExistingDirectory(self, "Select Directory")
         if directory:
             self.dirLineEdit.setText(directory)
 
     def loadAndPreprocessData(self):
-        path = self.dirLineEdit.text()
         self.preprocessing_params = {
             'flip': [self.horizontalFlip.isChecked(), self.verticalFlip.isChecked()],
             'signal_range' : {
@@ -150,31 +151,24 @@ class CardioBioEvalWidget(QWidget):
             'drift_correction': {
             'threshold': self.threshold.value(),
             'filter_method': self.filterMethod.currentText(),
-            'background_selector': self.backgroundSelector.isChecked(), #manual background selection is not implemented yet
+            'background_selector': False,
             }
         }
 
-        self.RESULT_PATH = os.path.join(path, 'result')
-        if not os.path.exists(self.RESULT_PATH):
-            os.mkdir(self.RESULT_PATH)
+        loader = self.loadAndPreprocessData_thread()
+        loader.finished.connect(self.loadAndPreprocessData_GUI)
+        loader.start()
 
-        self.raw_wells, self.full_time, self.full_phases = load_data(path, flip=self.preprocessing_params['flip'])
-        self.filter_params, _, _ = load_params(self.RESULT_PATH)
-        self.well_data, self.time, self.phases, self.filter_ptss, self.selected_range = preprocessing(self.preprocessing_params, self.raw_wells, self.full_time, self.full_phases, self.filter_params)
+    # Manual Background selection here if the automatic is bad
+    def manualBackgroundSelection(self):
+        self.preprocessing_params['drift_correction']['background_selector'] = True
+
+        self.clear_layers()
 
         for name in WELL_NAMES:
             visible = (name == WELL_NAMES[0])
             self.viewer.add_image(self.well_data[name], name=name, colormap='viridis', visible=visible)
-
-    # Manual Background selection here if the automatic is bad
-
-    # if self.backgroundSelector.isChecked():
-
-        # if self.preprocessing_params['drift_correction']['background_selector']:
-        #     background_selector = WellArrayBackgroundSelector(well_data, filter_params, False)
-
-        # if self.preprocessing_params['drift_correction']['background_selector']:
-        #     filter_ptss = background_selector.selected_coords
+            self.viewer.add_points(self.invert_coords(self.filter_ptss[name]), name=name + ' bg points', size=1, face_color='orange', visible=visible)
 
 
     def peakDetection(self):
@@ -183,18 +177,112 @@ class CardioBioEvalWidget(QWidget):
             'neighbourhood_size': self.neighbourhood_size.value(),
             'error_mask_filtering': self.errorMaskFiltering.isChecked()
         }
-        # Here the well data contains tha wells, the selected points and the filter points (which are the background points)
-        self.well_data = localization(self.preprocessing_params, self.localization_params, 
-                                self.raw_wells, self.selected_range, 
-                                {} if not self.preprocessing_params['drift_correction']['background_selector'] else background_selector.selected_coords)
+
+        peak_detector = self.peakDetection_thread()
+        peak_detector.finished.connect(self.peakDetection_GUI)
+        peak_detector.start()
+
+    def exportData(self):
+        self.export_params = {
+            'coordinates': self.coordinates.isChecked(),
+            'preprocessed_signals': self.preprocessedSignals.isChecked(),
+            'raw_signals': self.rawSignals.isChecked(),
+            'average_signal': self.averageSignal.isChecked(),
+            'breakdown_signal': self.breakdownSignal.isChecked(),
+            'max_well': self.maxWell.isChecked(),
+            'plot_signals_with_well': self.plotSignalsWithWell.isChecked(),
+            'plot_well_with_coordinates': self.plotWellWithCoordinates.isChecked(),
+            'plot_cells_individually': self.plotCellsIndividually.isChecked(),
+            'signal_parts_by_phases': self.signalPartsByPhases.isChecked(),
+            'max_centered_signals': self.maxCenteredSignals.isChecked()
+        }
 
         self.remaining_wells = self.remaining_wells_from_layers()
+        self.selected_ptss = self.get_selected_points()
 
+        for name in self.remaining_wells:
+            self.well_data[name] = (self.viewer.layers[name].data, self.selected_ptss[name], self.well_data[name][-1])
+
+        save_params(self.RESULT_PATH, self.well_data, self.preprocessing_params, self.localization_params)
+
+        exporter = self.export_res()
+        exporter.finished.connect(lambda: print('Export finished!'))
+        # exporter.finished.connect(lambda: self.viewer.notifications.show('Export finished!'))
+        exporter.start()
+
+        
+    def clear_layers(self):
         self.viewer.layers.select_all()
         self.viewer.layers.remove_selected()
 
-        # TODO kiválasztott sejtről a plot
+    def invert_coords(self, coords):
+        return np.array([[y, x] for x, y in coords])
+
+    def set_buttons_enabled(self, state):
+        self.loadButton.setEnabled(state)
+        self.backgroundSelectorButton.setEnabled(state)
+        self.peakButton.setEnabled(state)
+        self.exportButton.setEnabled(state)
+
+    def get_filter_points(self):
+        filter_ptss = {}
+        for name in WELL_NAMES:
+            filter_ptss[name] = self.invert_coords(np.round(self.viewer.layers[name + ' bg points'].data)).astype(np.uint8)
+        return filter_ptss
+
+    def get_selected_points(self):
+        selected_ptss = {}
+        for name in self.remaining_wells:
+            selected_ptss[name] = self.invert_coords(np.round(self.viewer.layers[name + ' peaks'].data)).astype(np.uint8)
+        return selected_ptss
+
+    def remaining_wells_from_layers(self):
+        remaining_wells = []
+        for layer in self.viewer.layers:
+            # if 'peaks' not in layer.name or 'bg' not in layer.name:
+            if  len(layer.name.split()) == 1:
+                remaining_wells.append(layer.name)
+        return remaining_wells        
+
+    @thread_worker
+    def loadAndPreprocessData_thread(self):
+        self.set_buttons_enabled(False)
+        self.loadButton.setEnabled(True)
+ 
+        path = self.dirLineEdit.text()
+        self.RESULT_PATH = os.path.join(path, 'result')
+        if not os.path.exists(self.RESULT_PATH):
+            os.mkdir(self.RESULT_PATH)
+
+        self.raw_wells, self.full_time, self.full_phases = load_data(path, flip=self.preprocessing_params['flip'])
+        self.filter_params, _, _ = load_params(self.RESULT_PATH)
+        self.well_data, self.time, self.phases, self.filter_ptss, self.selected_range = preprocessing(self.preprocessing_params, self.raw_wells, self.full_time, self.full_phases, self.filter_params)
         
+        self.set_buttons_enabled(True)
+
+    def loadAndPreprocessData_GUI(self):
+        self.clear_layers()
+        for name in WELL_NAMES:
+            visible = (name == WELL_NAMES[0])
+            self.viewer.add_image(self.well_data[name], name=name, colormap='viridis', visible=visible)
+
+    @thread_worker
+    def peakDetection_thread(self):
+        self.set_buttons_enabled(False)
+
+        if self.preprocessing_params['drift_correction']['background_selector']:
+            self.filter_ptss = self.get_filter_points()
+
+        # Here the well data contains tha wells, the selected points and the filter points (which are the background points)
+        self.well_data = localization(self.preprocessing_params, self.localization_params, 
+                                    self.raw_wells, self.selected_range, 
+                                    self.filter_ptss)
+
+        self.remaining_wells = self.remaining_wells_from_layers()
+        self.set_buttons_enabled(True)
+
+    def peakDetection_GUI(self):
+        self.clear_layers()
         # visualize the data with peaks
         for name in self.remaining_wells:
             visible = (name == self.remaining_wells[0])
@@ -226,51 +314,7 @@ class CardioBioEvalWidget(QWidget):
                     line.figure.canvas.draw()
                 except IndexError:
                     pass
-        
 
-    def exportData(self):
-        self.export_params = {
-            'coordinates': self.coordinates.isChecked(),
-            'preprocessed_signals': self.preprocessedSignals.isChecked(),
-            'raw_signals': self.rawSignals.isChecked(),
-            'average_signal': self.averageSignal.isChecked(),
-            'breakdown_signal': self.breakdownSignal.isChecked(),
-            'max_well': self.maxWell.isChecked(),
-            'plot_signals_with_well': self.plotSignalsWithWell.isChecked(),
-            'plot_well_with_coordinates': self.plotWellWithCoordinates.isChecked(),
-            'plot_cells_individually': self.plotCellsIndividually.isChecked(),
-            'signal_parts_by_phases': self.signalPartsByPhases.isChecked(),
-            'max_centered_signals': self.maxCenteredSignals.isChecked()
-        }
-
-        self.remaining_wells = self.remaining_wells_from_layers()
-        self.selected_ptss = self.selected_points()
-
-        for name in self.remaining_wells:
-            self.well_data[name] = (self.viewer.layers[name].data, self.selected_ptss[name], self.well_data[name][-1])
-
-        exporter = self.export_res()
-        exporter.finished.connect(lambda: print('Export finished'))
-        exporter.start()
-
-        save_params(self.RESULT_PATH, self.well_data, self.preprocessing_params, self.localization_params)
-        
-
-    def invert_coords(self, coords):
-        return np.array([[y, x] for x, y in coords])
-
-    def selected_points(self):
-        selected_ptss = {}
-        for name in self.remaining_wells:
-            selected_ptss[name] = self.invert_coords(np.round(self.viewer.layers[name + ' peaks'].data)).astype(np.uint8)
-        return selected_ptss
-
-    def remaining_wells_from_layers(self):
-        remaining_wells = []
-        for layer in self.viewer.layers:
-            if 'peaks' not in layer.name:
-                remaining_wells.append(layer.name)
-        return remaining_wells        
 
     @thread_worker
     def export_res(self):
